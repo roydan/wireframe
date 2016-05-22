d2c7ab1b4fe4dc95daea2cb047f44157e53d8422
Daniel Roy <d.roy@atos.net>
2016-05-22 08:17:20 -0400

    add guicon.*, remove Matrix4x4.*

---
+++ guicon.h
@@ -0,0 +1,12 @@
+/**********************************************************
+ *
+ * guicon.h
+ * Interface to console redirection function
+ *
+ *********************************************************/
+#ifndef __GUICON_H__
+#define __GUICON_H__
+
+void RedirectIOToConsole();
+
+#endif	// __GUICON_H__
